17088cdc14e4669673822149a55fa28e352d604b
Peter Kona <ikona23@gmail.com>
2016-07-30 02:23:47 +1200

    total count

--- game.js
+++ game.js
@@ -17,6 +17,24 @@ function bindEventListeners (dots) {
   }
 }
 
+function makeGreen (evt) {
+  evt.preventDefault();
+  evt.target.classList.toggle('green');
+  updateCounts();
+}
+//
+
+// CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue');
+  updateCounts();
+}
+// CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible');
+  updateCounts();
+}
+
 function updateCounts () {
   var totals = {
     blue: 0,
@@ -39,24 +57,6 @@ function updateCounts () {
     // Once you've done the counting, this function will update the display
     displayTotals(totals);
   }
-
-function makeGreen (evt) {
-  evt.preventDefault();
-  evt.target.classList.toggle('green');
-  updateCounts();
-}
-//
-
-// CREATE FUNCTION makeBlue HERE
-function makeBlue (evt) {
-  evt.target.classList.toggle('blue');
-  updateCounts();
-}
-// CREATE FUNCTION hide HERE
-function hide (evt) {
-  evt.target.classList.toggle('invisible');
-  updateCounts();
-}
 
 function displayTotals (totals) {
   for (var key in totals) {
